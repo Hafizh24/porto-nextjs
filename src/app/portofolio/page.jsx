99184de1b45eed5a42bf5d68d8a0338f6938f523
Hafizh24 <hafizhcbs@gmail.com>
2024-03-07 22:35:11 +0700

    feat: add porto page

---
+++ src/app/portofolio/page.jsx
@@ -0,0 +1,13 @@
+import PortofolioView from './view'
+import Navbar from '../components/Navbar'
+
+const PortofolioPage = () => {
+  return (
+    <>
+      <Navbar />
+      <PortofolioView />
+    </>
+  )
+}
+
+export default PortofolioPage
